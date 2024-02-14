cd6e35535cf354589c24b94af57f56e6b61eaad5
Lars Simon Markhus <lars.simon.markhus@webstep.no>
2024-02-14 12:25:56 +0100

    Exercise 3.6

--- index.js
+++ index.js
@@ -56,6 +56,18 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
   const person = {
     id: Math.floor(Math.random() * 9999999999),
     name: body.name,
