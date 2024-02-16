8963f711d7a7a2e325e59b3ac1655dee57a86c16
Lars Simon Markhus <lars.simon.markhus@webstep.no>
2024-02-16 13:57:40 +0100

    Exercise 3.16

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
-app.use(express.static("dist"));
 const Person = require("./models/person");
 
 morgan.token("body", function (req, res) {
@@ -10,12 +9,25 @@ morgan.token("body", function (req, res) {
   else return " ";
 });
 
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
 
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 let persons = [
   {
@@ -40,10 +52,12 @@ let persons = [
   },
 ];
 
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then((result) => {
-    response.json(result);
-  });
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/info", (request, response) => {
@@ -63,7 +77,7 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
   if (!body.name) {
     return response.status(400).json({
@@ -77,28 +91,29 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  // if (persons.find((person) => person.name === body.name)) {
-  //   return response.status(400).json({
-  //     error: "name must be unique",
-  //   });
-  // }
-
   const person = new Person({
     id: Math.floor(Math.random() * 9999999999),
     name: body.name,
     number: body.number ? body.number : "None",
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+
   const id = Number(request.params.id);
   persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
 });
 
 const PORT = 3001;
