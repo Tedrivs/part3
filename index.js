9c22f8216a6b5cec0eb687f2c8fe2901f4452ab0
Lars Simon Markhus <lars.simon.markhus@webstep.no>
2024-02-20 09:43:43 +0100

    Exercise 3.22

--- index.js
+++ index.js
@@ -1,122 +1,120 @@
-const express = require("express");
-const morgan = require("morgan");
-const app = express();
-const cors = require("cors");
-const Person = require("./models/person");
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+const Person = require('./models/person')
 
-morgan.token("body", function (req, res) {
-  if (req.method === "POST") return JSON.stringify(req.body);
-  else return " ";
-});
+morgan.token('body', function (req) {
+  if (req.method === 'POST') return JSON.stringify(req.body)
+  else return ' '
+})
 
-app.use(express.static("dist"));
-app.use(express.json());
-app.use(cors());
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(cors())
 
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 const errorHandler = (error, request, response, next) => {
-  console.log(error.name);
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  console.log(error.name)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-  next(error);
-};
+  next(error)
+}
 
-app.get("/api/persons", (request, response, next) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({})
     .then((result) => {
-      response.json(result);
+      response.json(result)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/api/info", (request, response, next) => {
-  let personLength = 0;
+app.get('/api/info', (request, response, next) => {
+  let personLength = 0
   Person.find({})
     .then((result) => {
-      personLength = result.length;
-      const info = `Phonebook has info for ${personLength} people </br></br>${new Date()}`;
-      response.send(info);
+      personLength = result.length
+      const info = `Phonebook has info for ${personLength} people </br></br>${new Date()}`
+      response.send(info)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
   if (!body.name) {
     return response.status(400).json({
-      error: "name missing",
-    });
+      error: 'name missing',
+    })
   }
 
   if (!body.number) {
     return response.status(400).json({
-      error: "number missing",
-    });
+      error: 'number missing',
+    })
   }
 
   const person = new Person({
     id: Math.floor(Math.random() * 9999999999),
     name: body.name,
-    number: body.number ? body.number : "None",
-  });
+    number: body.number ? body.number : 'None',
+  })
 
   person
     .save()
     .then((savedPerson) => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
   const person = {
     name: body.name,
     number: body.number,
-  };
+  }
   Person.findByIdAndUpdate(request.params.id, person, {
     new: true,
     runValidators: true,
-    context: "query",
+    context: 'query',
   })
     .then((updatedPerson) => {
-      response.json(updatedPerson);
+      response.json(updatedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then((result) => {
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
-    .catch((error) => next(error));
+    .catch((error) => next(error))
+})
 
-  const id = Number(request.params.id);
-});
+app.use(errorHandler)
 
-app.use(errorHandler);
-
-const PORT = 3001;
+const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- models/person.js
+++ models/person.js
@@ -1,20 +1,20 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minLength: 3, required: true },
@@ -22,20 +22,20 @@ const personSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: function (v) {
-        return /^\d{2,3}-\d+(?:(?!-)\d)*$/.test(v);
+        return /^\d{2,3}-\d+(?:(?!-)\d)*$/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
     required: true,
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
